4fca78df1d79818a41da07f8e9918faf61d699a5
varaprasad220 <162006447+varaprasad220@users.noreply.github.com>
2024-07-30 11:06:34 +0530

    Create amazon-linux-minikube.sh

---
+++ amazon-linux-minikube.sh
@@ -0,0 +1,15 @@
+yum install docker -y
+systemctl enable docker
+systemctl start docker
+systemctl status docker
+docker --version
+yum install conntrack -y
+yum install git -y
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo mv minikube-linux-amd64 /usr/local/bin/minikube
+sudo chmod +x /usr/local/bin/minikube
+sudo minikube version
+sudo minikube start --force --driver=docker
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo mv kubectl /usr/local/bin/kubectl
+sudo chmod +x /usr/local/bin/kubectl
